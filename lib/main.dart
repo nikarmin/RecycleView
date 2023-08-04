32e47ea07144db62ff08d656488192a7ed9cb58a
nikarmin <nikafworks@gmail.com>
2023-08-04 12:17:32 -0300

    novas páginas...

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:recycle_view/views/cadastro.dart';
+import 'package:recycle_view/views/educacao_ambiental_page.dart';
+import 'package:recycle_view/views/education/materiais_eletronicos_page.dart';
+import 'package:recycle_view/views/education/oleo_page.dart';
+import 'package:recycle_view/views/education/papel_page.dart';
+import 'package:recycle_view/views/education/res_hospitalares_page.dart';
+import 'package:recycle_view/views/education/res_radioativos_page.dart';
 import 'package:recycle_view/views/login.dart';
 import 'package:recycle_view/views/tela_inicial.dart';
 import 'package:recycle_view/views/bemvindo_page.dart';
 import 'package:recycle_view/views/cadastro_de_pontos_page.dart';
 import 'package:recycle_view/views/graficos_page.dart';
+import 'dart:io';
 
 void main() async {
   runApp(MainApp());
@@ -20,7 +27,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       color: Color.fromRGBO(233, 233, 233, 1),
       debugShowCheckedModeBanner: false,
-      home: TelaInicial(),
+      home: EducacaoAmbientalPage(),
     );
   }
 }
---
+++ lib/views/education/materiais_eletronicos_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class MatEletronicosPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: ImageIcon(AssetImage('assets/images/icons/earth-love.png'),
+            color: Colors.black),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(245, 245, 245, 1),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: ImageIcon(
+              AssetImage('assets/images/icons/profile.png'),
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background/eletronicos_page.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+      backgroundColor: Color.fromRGBO(233, 233, 233, 1),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(color: Colors.black12, spreadRadius: 0, blurRadius: 10)
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: BottomNavigationBar(
+            currentIndex: 0,
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedItemColor: Color.fromARGB(255, 89, 165, 128),
+            onTap: (value) {},
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage(
+                    'assets/images/icons/home.png',
+                  ),
+                  size: 50,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icons/camera.png'),
+                    size: 50,
+                  ),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icons/newspaper.png'),
+                    size: 50,
+                  ),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icons/pin.png'),
+                    size: 50,
+                  ),
+                  label: ''),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/icons/planet-ecologic.png'),
+                    size: 50,
+                  ),
+                  label: ''),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
